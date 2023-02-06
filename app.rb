b3d7b230fc3ceb09bedcfeae4c7711a11c385a21
guantai <jpguantai@gmail.com>
2023-02-06 13:38:21 +0300

    Assigment Complete

--- app.rb
+++ app.rb
@@ -14,10 +14,39 @@
 # # 1 + 1
 # puts add + add
 
-def add num1, num2
-    puts num1
-    puts num2
-    num1 + num2
-end
+# def add num1, num2
+#     puts num1
+#     puts num2
+#     num1 + num2
+# end
 
-puts add(1,3) + add(2,2)
+
+# puts add(1,3) + add(2,2)
+
+puts "Hello World"
+
+print "Pass this test, please."
+
+p [1,2,3]
+
+puts "Hello World"
+
+# print "Hello World"
+# print "Hello World"
+# print "Hello World"
+
+# it 'outputs the string "Hello World!" using the puts method' do
+#     # load 'app.rb' will run the code in the app.rb file
+#     # \n represents a newline character (remember, puts will include a line break at the end of the string)
+#     # stdout represents the output of your program
+#     expect { load 'app.rb' }.to output(a_string_including("Hello World!\n")).to_stdout
+#   end
+
+puts "Hello World!\n"
+
+# numbers
+# puts 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+
+# p [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
